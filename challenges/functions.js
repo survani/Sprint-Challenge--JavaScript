c877fd05ca524ac13acc0991d96ac24d761f68b9
Giovani Garfias <giovaniportal@gmail.com>
2019-10-11 19:12:17 -0700

    Finished functions.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,6 +6,11 @@
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
+//! My code starts here:
+
+let consume = (a, b, cb) => {
+  return cb(a, b);
+}
 
 
 /* Step 2: Create several functions to callback with consume();
@@ -13,20 +18,32 @@
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+//! My code starts here:
 
+let add = (a, b) => {
+  return a + b;
+};
+
+let multiply = (a, b) => {
+  return a * b;
+};
+
+let greeting = (firstName, lastName) => {
+  return `Hello ${firstName} ${lastName}, nice to meet you!`;
+};
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+ console.log(consume(2, 2, add)); // 4
+ console.log(consume(10, 16, multiply)); // 160
+ console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
+ // ABOVE WORKS & COMPLETED!!! ----------------------------------------------------------------------------
 
 // ==== Closures ==== 
 
 // Explain in your own words why nestedfunction can access the variable internal.
-
 // Explanation: 
-
+//ANSWER: Lexical Scope makes the nestedfunction able to access the variable internally. 
 
 const external = "I'm outside the function";
 
